d0d4a1349e0e28b387309900c17dc935a9154d0b
Dale <djblack@uci.edu>
2021-06-30 15:03:30 -0700

    delete

--- src/utils.jl
+++
@@ -1,9 +0,0 @@
-# TODO: import this directly from DistanceTransforms.jl
-function compute_dtm(img)
-    f = ImageMorphology.feature_transform(.!(Bool.(img)))
-    foreground_dtm = ImageMorphology.distance_transform(f)
-end
-
-function get_curvature(ϕ, idx)
-    [dim_y, dim_x] = size(ϕ)
-end
