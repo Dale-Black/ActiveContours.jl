203a08e5158dcc5b5fad5d7fb1e7d088409e9a2f
Dale-Black <djblack@uci.edu>
2023-01-26 12:43:15 -0800

    update format

--- src/chanvese_py.jl
+++ src/chanvese_py.jl
@@ -7,8 +7,9 @@ using InteractiveUtils
 # ╔═╡ 0cf5da71-cddb-4143-8bed-184eb98d538f
 # ╠═╡ show_logs = false
 begin
-	using Pkg; Pkg.activate("..")
-	using Revise, PlutoUI, PlutoTest, Images, Statistics, ActiveContours
+    using Pkg
+    Pkg.activate("..")
+    using Revise, PlutoUI, PlutoTest, Images, Statistics, ActiveContours
 end
 
 # ╔═╡ 79db70bb-b291-459a-9b91-9dfabbaee514
@@ -32,15 +33,15 @@ _cv_curvature(phi)
 """
 function _cv_curvature(phi)
     P = padarray(phi, Pad(:replicate, 1, 1))
-	fy = (P[2:end, 1:end-1] - P[0:end-2, 1:end-1]) / 2.0
+    fy = (P[2:end, 1:end-1] - P[0:end-2, 1:end-1]) / 2.0
     fx = (P[1:end-1, 2:end] - P[1:end-1, 0:end-2]) / 2.0
     fyy = P[2:end, 1:end-1] + P[0:end-2, 1:end-1] - 2 * phi
     fxx = P[1:end-1, 2:end] + P[1:end-1, 0:end-2] - 2 * phi
     fxy = 0.25 * (P[2:end, 2:end] + P[0:end-2, 0:end-2] - P[0:end-2, 2:end] - P[2:end, 0:end-2])
-    grad2 = fx.^2 + fy.^2
-	K_num = (fxx .* fy.^2 - 2 * fxy .* fx .* fy + fyy .* fx.^2)
+    grad2 = fx .^ 2 + fy .^ 2
+    K_num = (fxx .* fy .^ 2 - 2 * fxy .* fx .* fy + fyy .* fx .^ 2)
     K_den = (grad2 .* sqrt.(grad2) .+ 1e-8)
-	K = K_num ./ K_den
+    K = K_num ./ K_den
     return K
 end
 
@@ -77,7 +78,7 @@ _cv_delta(x, eps=1.0)
 Returns the result of a regularised dirac delta function of the input value(s).
 """
 function _cv_delta(x, eps=1.0)
-    eps ./ (eps.^2 .+ x.^2)
+    eps ./ (eps .^ 2 .+ x .^ 2)
 end
 
 # ╔═╡ 9d392007-421e-4bea-80ed-c321bac2043c
@@ -91,7 +92,7 @@ Returns the variation of level set 'phi' based on algorithm parameters.
 function _cv_calculate_variation(image, phi, mu, lambda1, lambda2, dt)
     eta = 1e-16
     P = padarray(phi, Pad(:replicate, 1, 1))
-	
+
     phixp = P[1:end-1, 2:end] - P[1:end-1, 1:end-1]
     phixn = P[1:end-1, 1:end-1] - P[1:end-1, 0:end-2]
     phix0 = (P[1:end-1, 2:end] - P[1:end-1, 0:end-2]) / 2.0
@@ -100,21 +101,21 @@ function _cv_calculate_variation(image, phi, mu, lambda1, lambda2, dt)
     phiyn = P[1:end-1, 1:end-1] - P[0:end-2, 1:end-1]
     phiy0 = (P[2:end, 1:end-1] - P[0:end-2, 1:end-1]) / 2.0
 
-    C1 = 1 ./ sqrt.(eta .+ phixp.^2 .+ phiy0.^2)
-    C2 = 1 ./ sqrt.(eta .+ phixn.^2 .+ phiy0.^2)
-    C3 = 1 ./ sqrt.(eta .+ phix0.^2 .+ phiyp.^2)
-    C4 = 1 ./ sqrt.(eta .+ phix0.^2 .+ phiyn.^2)
+    C1 = 1 ./ sqrt.(eta .+ phixp .^ 2 .+ phiy0 .^ 2)
+    C2 = 1 ./ sqrt.(eta .+ phixn .^ 2 .+ phiy0 .^ 2)
+    C3 = 1 ./ sqrt.(eta .+ phix0 .^ 2 .+ phiyp .^ 2)
+    C4 = 1 ./ sqrt.(eta .+ phix0 .^ 2 .+ phiyn .^ 2)
 
     K = P[1:end-1, 2:end] .* C1 .+ P[1:end-1, 0:end-2] .* C2 .+ P[2:end, 1:end-1] .* C3 .+ P[0:end-2, 1:end-1] .* C4
 
     Hphi = 1 .* (phi .> 0)
     c1, c2 = _cv_calculate_averages(image, Hphi)
 
-    difference_from_average_term = -1 .* lambda1 .* (image .- c1).^2 .+ lambda2 .* (image .- c2).^2
-	
-    new_phi = phi + (dt*_cv_delta(phi)) .* (mu*K + difference_from_average_term)
-	
-	return new_phi ./ (1 .+ mu .* dt .* _cv_delta(phi) .* (C1+C2+C3+C4))
+    difference_from_average_term = -1 .* lambda1 .* (image .- c1) .^ 2 .+ lambda2 .* (image .- c2) .^ 2
+
+    new_phi = phi + (dt * _cv_delta(phi)) .* (mu * K + difference_from_average_term)
+
+    return new_phi ./ (1 .+ mu .* dt .* _cv_delta(phi) .* (C1 + C2 + C3 + C4))
 end
 
 # ╔═╡ 7aadad4a-5025-407b-a60f-769b2fda6199
@@ -126,7 +127,7 @@ _cv_heavyside(x, eps=1)
 Returns the result of a regularised heavyside function of the input value(s).
 """
 function _cv_heavyside(x, eps=1)
-	return 0.5 .* (1. .+ (2 ./ pi) .* atan.(x ./ eps))
+    return 0.5 .* (1.0 .+ (2 ./ pi) .* atan.(x ./ eps))
 end
 
 # ╔═╡ 4c1907d9-cf8c-448b-945f-9b1afd9572f5
@@ -182,14 +183,14 @@ function chan_vese(image, phi; mu=0.25, lambda1=1, lambda2=1, tol=1e-3, max_iter
     phivar = tol + 1
     segmentation = phi .> 0
 
-    while(phivar > tol && i < max_iter)
+    while (phivar > tol && i < max_iter)
         # Save old level set values
         oldphi = phi
 
         # Calculate new level set
         phi = _cv_calculate_variation(image, phi, mu, lambda1, lambda2, dt)
         # phi = _cv_reset_level_set(phi)
-        phivar = sqrt(mean((phi-oldphi).^2))
+        phivar = sqrt(mean((phi - oldphi) .^ 2))
 
         # Extract energy and compare to previous level set and
         # segmentation to see if continuing is necessary
@@ -214,30 +215,30 @@ md"""
 
 # ╔═╡ 5e82784b-3727-4987-b033-1360c9a73795
 test_image = [
-  1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0
-  0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0
- -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0
- -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0
-  1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0
-  0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0
- -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0
- -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0
-  1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0
-  0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0
+    1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0
+    0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0
+    -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0
+    -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0
+    1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0
+    0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0
+    -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0
+    -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0
+    1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0
+    0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0
 ]
 
 # ╔═╡ 4c2ad612-1587-4669-af61-27c473532739
 test_phi = [
-  1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0
-  0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0
- -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0
- -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0
-  1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0
-  0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0
- -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0
- -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0
-  1.0   0.0  -1.0  -0.0   1.0   0.0  -1.0  -0.0   1.0   0.0
-  0.0   0.0  -0.0  -0.0   0.0   0.0  -0.0  -0.0   0.0   0.0
+    1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0
+    0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0
+    -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0
+    -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0
+    1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0
+    0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0
+    -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0
+    -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0
+    1.0 0.0 -1.0 -0.0 1.0 0.0 -1.0 -0.0 1.0 0.0
+    0.0 0.0 -0.0 -0.0 0.0 0.0 -0.0 -0.0 0.0 0.0
 ]
 
 # ╔═╡ f21c1301-0a52-4454-a588-e8bb1195072b
@@ -245,29 +246,29 @@ test_Hphi = 1 * (test_phi .> 0)
 
 # ╔═╡ c619f055-f64c-4766-9ca3-11136313121a
 begin
-	test_mu = 0.25
-	test_lambda1 = 1
-	test_lambda2 = 1
-	test_dt = 0.5
+    test_mu = 0.25
+    test_lambda1 = 1
+    test_lambda2 = 1
+    test_dt = 0.5
 end
 
 # ╔═╡ aed2424b-4e9c-4b1f-b802-9b858558516a
 let
     phi = init_checkerboard(size(test_image), 5)
-	segmentation, phi, energies = chan_vese(test_image, phi; max_iter=200);
-	ans = [
-		 1  1  1  1  0  0  0  0  0  0
-		 1  1  1  1  0  0  0  0  0  0
-		 1  1  1  1  0  0  0  0  0  0
-		 1  1  1  1  0  0  0  0  0  0
-		 0  0  0  0  0  0  0  0  0  0
-		 0  0  0  0  0  0  0  0  0  0
-		 0  0  0  0  0  0  0  0  0  0
-		 0  0  0  0  0  0  0  0  0  0
-		 0  0  0  0  0  0  0  0  1  1
-		 0  0  0  0  0  0  0  0  1  1
-	]
-	@test segmentation == ans
+    segmentation, phi, energies = chan_vese(test_image, phi; max_iter=200)
+    ans = [
+        1 1 1 1 0 0 0 0 0 0
+        1 1 1 1 0 0 0 0 0 0
+        1 1 1 1 0 0 0 0 0 0
+        1 1 1 1 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 0 0
+        0 0 0 0 0 0 0 0 1 1
+        0 0 0 0 0 0 0 0 1 1
+    ]
+    @test segmentation == ans
 end
 
 # ╔═╡ Cell order:
