641be0fd7058eb32d504ec856495931d7626d2ca
Dale-Black <djblack@uci.edu>
2023-02-01 14:31:38 -0800

    delete normalize from utils

--- src/utils.jl
+++
@@ -1,7 +0,0 @@
-"""
-    normalize(img)
-Normalize an image based on the mean and the standard deviation
-
-# TODO: check this
-"""
-normalize(img) = (img .- minimum(img)) ./ (maximum(img) .- minimum(img))
