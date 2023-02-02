58a4625c2d252acaa758369e46fcd07d61bd580c
Dale-Black <djblack@uci.edu>
2023-02-02 10:15:32 -0800

    simplify init checkerboard

--- src/inits.jl
+++ src/inits.jl
@@ -1,5 +1,5 @@
 ### A Pluto.jl notebook ###
-# v0.19.18
+# v0.19.22
 
 using Markdown
 using InteractiveUtils
@@ -16,23 +16,19 @@ TableOfContents()
 
 # ╔═╡ 18da3cf3-c3a5-4d43-a584-dc0b63cf7a8e
 function init_checkerboard(image_size, square_size)
-	yv = reshape(collect(range(1, image_size[1])), (image_size[1], 1))
-    xv = collect(range(1, image_size[2]))
-    sf = pi / square_size
-    xv = xv .* sf
-    yv = yv .* sf
-    return sin.(yv) .* sin.(xv)'
+	rows, cols = image_size
+	sf = pi / square_size
+	
+	yv = sin.(collect(1:rows)' .* sf)
+	xv = sin.(collect(1:cols) .* sf)
+    return yv .* xv
 end
 
 # ╔═╡ 00ee61b7-bd99-4a01-a985-af6772f02a95
 export init_checkerboard
-
-# ╔═╡ 7c608bbf-0105-4036-8e37-6adddc0fc7d6
-round.(init_checkerboard((10, 10), 2))
 
 # ╔═╡ Cell order:
 # ╠═a6b0911a-9db4-11ed-2e08-e10ba1344306
 # ╠═112cb9ba-6dd0-4881-a67e-b104dc1a38c5
 # ╠═18da3cf3-c3a5-4d43-a584-dc0b63cf7a8e
 # ╠═00ee61b7-bd99-4a01-a985-af6772f02a95
-# ╠═7c608bbf-0105-4036-8e37-6adddc0fc7d6
